842bcad71d861a2481ca4754bee47893e84f8d7d
Hiroyuki Ikegami <ikegam@mixallow.net>
2010-01-27 22:20:36 +0900

    fix mergine

--- lib/aastring.rb
+++ lib/aastring.rb
@@ -5,18 +5,19 @@ class String
   require 'pango'
   require 'tempfile'
 
-  def to_aa(height=400)
+  def to_aa(height=12)
     ret = ""
     self.split(/\n/).each{|line|
-      surface = Cairo::ImageSurface.new(Cairo::FORMAT_ARGB32, height, 18)
+      surface = Cairo::ImageSurface.new(Cairo::FORMAT_ARGB32, 400, height + (height/3).to_f)
       context = Cairo::Context.new(surface)
       context.set_source_rgb(1, 1, 1)
-      context.rectangle(0, 0, height, 18)
+      context.rectangle(0, 0, 400, height + (height/3).to_f)
       context.fill
       context = Cairo::Context.new(surface)
       layout = context.create_pango_layout
       layout.text = line
-      layout.width = height * Pango::SCALE
+      layout.font_description = "Sans #{height}"
+      layout.width = 400 * Pango::SCALE
       context.show_pango_layout(layout)
       temp = Tempfile.new("termpresenter", "/tmp")
       surface.write_to_png(temp.path)
@@ -28,6 +29,13 @@ class String
 end
 
 if $0 == __FILE__
-  "ああああ\nあああ\n池上洋行\nあいうえお".to_aa.split(/\n/).each{|line| puts line }
+  puts "ああああ".to_aa.split(/\n/).size + 1
+  puts "ああああ".to_aa(13).split(/\n/).size + 1
+  puts "ああああ".to_aa(14).split(/\n/).size + 1
+  puts "ああああ".to_aa(15).split(/\n/).size + 1
+  puts "ああああ".to_aa(16).split(/\n/).size + 1
+  puts "ああああ".to_aa(17).split(/\n/).size + 1
+  puts "ああああ".to_aa(18).split(/\n/).size + 1
+  puts "ああああ".to_aa(18)
+  puts "ああああ".to_aa(30)
 end
-
